1de1ef1fd73d99c84136763d8002cf8854018540
sandra-sanj <sandra.juursoo@metropolia.fi>
2026-02-04 10:09:45 +0200

    add TemperatureConverter class and related tests

--- src/main/java/temperature_converter/TemperatureConverter.java
+++ src/main/java/temperature_converter/TemperatureConverter.java
@@ -1,4 +1,21 @@
 package temperature_converter;
 
 public class TemperatureConverter {
+
+    public double fahrenheitToCelsius(double fahrenheit) {
+        double celcius = (fahrenheit - 32) * 5 / 9;
+        return Math.round(celcius * 10.0) / 10.0;
+    }
+
+    public double celsiusToFahrenheit(double celcius) {
+        double fahrenheit = (celcius * 9 / 5) + 32;
+        return Math.round(fahrenheit * 10.0) / 10.0;
+    }
+
+    boolean isExtremeTemperature(double celcius) {
+        if (-40 >= celcius || celcius >= 50) {
+            return true;
+        }
+        return false;
+    }
 }
--- src/test/java/temperature_converter/TemperatureConverterTest.java
+++ src/test/java/temperature_converter/TemperatureConverterTest.java
@@ -1,4 +1,123 @@
+package temperature_converter;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class TemperatureConverterTest {
-  
+    private static TemperatureConverter temperatureConverter;
+
+    @BeforeAll
+    static void setup() {
+        temperatureConverter = new TemperatureConverter();
+    }
+
+    @Test
+    void fahrenheitToCelsius_40() {
+        double fahrenheit = 40;
+        assertEquals(4.4, temperatureConverter.fahrenheitToCelsius(fahrenheit));
+    }
+
+    @Test
+    void fahrenheitToCelsius_0() {
+        double fahrenheit = 0;
+        assertEquals(-17.8, temperatureConverter.fahrenheitToCelsius(fahrenheit));
+    }
+
+    @Test
+    void fahrenheitToCelsius_32() {
+        double fahrenheit = 32;
+        assertEquals(0.0, temperatureConverter.fahrenheitToCelsius(fahrenheit));
+    }
+
+    @Test
+    void fahrenheitToCelsius_Negative11() {
+        double fahrenheit = -11;
+        assertEquals(-23.9, temperatureConverter.fahrenheitToCelsius(fahrenheit));
+    }
+
+    @Test
+    void celsiusToFahrenheit_0() {
+        double celcius = 0;
+        assertEquals(32.0, temperatureConverter.celsiusToFahrenheit(celcius));
+    }
+
+    @Test
+    void celsiusToFahrenheit_32() {
+        double celcius = 32;
+        assertEquals(89.6, temperatureConverter.celsiusToFahrenheit(celcius));
+    }
+
+    @Test
+    void celsiusToFahrenheit_40() {
+        double celcius = 40;
+        assertEquals(104.0, temperatureConverter.celsiusToFahrenheit(celcius));
+    }
+
+    @Test
+    void celsiusToFahrenheit_62() {
+        double celcius = 62;
+        assertEquals(143.6, temperatureConverter.celsiusToFahrenheit(celcius));
+    }
+
+    @Test
+    void celsiusToFahrenheit_100_1() {
+        double celcius = 100.1;
+        assertEquals(212.2, temperatureConverter.celsiusToFahrenheit(celcius));
+    }
+
+    @Test
+    void celsiusToFahrenheit_Negative10() {
+        double celcius = -10;
+        assertEquals(14.0, temperatureConverter.celsiusToFahrenheit(celcius));
+    }
+
+    @Test
+    void celsiusToFahrenheit_Negative15() {
+        double celcius = -15;
+        assertEquals(5.0, temperatureConverter.celsiusToFahrenheit(celcius));
+    }
+
+    @Test
+    void isExtremeTemperature_False_0() {
+        double celcius = 0;
+        assertFalse(temperatureConverter.isExtremeTemperature(celcius));
+    }
+
+    @Test
+    void isExtremeTemperature_False_20() {
+        double celcius = 20;
+        assertFalse(temperatureConverter.isExtremeTemperature(celcius));
+    }
+
+    @Test
+    void isExtremeTemperature_True_100() {
+        double celcius = 100;
+        assertTrue(temperatureConverter.isExtremeTemperature(celcius));
+    }
+
+    @Test
+    void isExtremeTemperature_True_50() {
+        double celcius = 50;
+        assertTrue(temperatureConverter.isExtremeTemperature(celcius));
+    }
+
+    @Test
+    void isExtremeTemperature_False_49_9() {
+        double celcius = 49.9;
+        assertFalse(temperatureConverter.isExtremeTemperature(celcius));
+    }
+
+    @Test
+    void isExtremeTemperature_True_Negative40() {
+        double celcius = -40;
+        assertTrue(temperatureConverter.isExtremeTemperature(celcius));
+    }
+
+    @Test
+    void isExtremeTemperature_False_39_9() {
+        double celcius = -39.9;
+        assertFalse(temperatureConverter.isExtremeTemperature(celcius));
+    }
 }
