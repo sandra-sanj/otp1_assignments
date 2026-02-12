dcf7c427d1585f4c53212728872bc5548d4125e6
sandra-sanj <sandra.juursoo@metropolia.fi>
2026-02-12 10:27:26 +0200

    add TemperatureConverter class and related tests

---
+++ TemperatureConverter/src/main/java/temperature_converter/Main.java
@@ -0,0 +1,28 @@
+package temperature_converter;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter temperature: ");
+        double temperature = sc.nextDouble();
+
+        System.out.print("Select temperature unit (c/f) and convert to the other: ");
+        String unit = sc.next();
+
+        TemperatureConverter temperatureConverter = new TemperatureConverter();
+        double convertedTemperature;
+        String convertedTemperatureUnit;
+
+        if (unit.equals("c")) {
+            convertedTemperature = temperatureConverter.celsiusToFahrenheit(temperature);
+            convertedTemperatureUnit = "F";
+        } else {
+            convertedTemperature = temperatureConverter.fahrenheitToCelsius(temperature);
+            convertedTemperatureUnit = "C";
+        }
+
+        System.out.println(temperature + " " + unit.toUpperCase() + " equals " + convertedTemperature + " " + convertedTemperatureUnit);
+    }
+}
