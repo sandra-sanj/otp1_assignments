d6b7f5e1d2106c9f100c212f286521e712f9930d
sandra-sanj <sandra.juursoo@metropolia.fi>
2026-02-23 13:45:43 +0200

    add tests for kelvinToCelsius method, modify other tests to use ParameterizedTest

--- TemperatureConverter/src/test/java/temperature_converter/TemperatureConverterTest.java
+++ TemperatureConverter/src/test/java/temperature_converter/TemperatureConverterTest.java
@@ -2,6 +2,8 @@ package temperature_converter;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,111 +15,27 @@ class TemperatureConverterTest {
         temperatureConverter = new TemperatureConverter();
     }
 
-    @Test
-    void fahrenheitToCelsius_40() {
-        double fahrenheit = 40;
-        assertEquals(4.4, temperatureConverter.fahrenheitToCelsius(fahrenheit));
+    @ParameterizedTest(name = "{0} Fahrenheit is {1} Celsius")
+    @CsvSource({"40, 4.4", "0, -17.8", "32, 0.0", "-11, -23.9"})
+    void fahrenheitToCelsius(double fahrenheit, double expectedCelsius) {
+        assertEquals(expectedCelsius, temperatureConverter.fahrenheitToCelsius(fahrenheit));
     }
 
-    @Test
-    void fahrenheitToCelsius_0() {
-        double fahrenheit = 0;
-        assertEquals(-17.8, temperatureConverter.fahrenheitToCelsius(fahrenheit));
+    @ParameterizedTest(name = "{0} Celsius is {1} Fahrenheit")
+    @CsvSource({"0, 32.0", "32, 89.6", "40, 104.0", "62, 143.6", "100.1, 212.2", "-10, 14.0", "-15, 5.0"})
+    void celsiusToFahrenheit(double celsius, double expectedFahrenheit) {
+        assertEquals(expectedFahrenheit, temperatureConverter.celsiusToFahrenheit(celsius));
     }
 
-    @Test
-    void fahrenheitToCelsius_32() {
-        double fahrenheit = 32;
-        assertEquals(0.0, temperatureConverter.fahrenheitToCelsius(fahrenheit));
+    @ParameterizedTest(name = "{0} Celsius extreme = {1}")
+    @CsvSource({"0, false", "20, false", "49.9, false", "-39.9, false", "50, true", "100, true", "-40, true"})
+    void isExtremeTemperature(double celsius, boolean expected) {
+        assertEquals(expected, temperatureConverter.isExtremeTemperature(celsius));
     }
 
-    @Test
-    void fahrenheitToCelsius_Negative11() {
-        double fahrenheit = -11;
-        assertEquals(-23.9, temperatureConverter.fahrenheitToCelsius(fahrenheit));
-    }
-
-    @Test
-    void celsiusToFahrenheit_0() {
-        double celcius = 0;
-        assertEquals(32.0, temperatureConverter.celsiusToFahrenheit(celcius));
-    }
-
-    @Test
-    void celsiusToFahrenheit_32() {
-        double celcius = 32;
-        assertEquals(89.6, temperatureConverter.celsiusToFahrenheit(celcius));
-    }
-
-    @Test
-    void celsiusToFahrenheit_40() {
-        double celcius = 40;
-        assertEquals(104.0, temperatureConverter.celsiusToFahrenheit(celcius));
-    }
-
-    @Test
-    void celsiusToFahrenheit_62() {
-        double celcius = 62;
-        assertEquals(143.6, temperatureConverter.celsiusToFahrenheit(celcius));
-    }
-
-    @Test
-    void celsiusToFahrenheit_100_1() {
-        double celcius = 100.1;
-        assertEquals(212.2, temperatureConverter.celsiusToFahrenheit(celcius));
-    }
-
-    @Test
-    void celsiusToFahrenheit_Negative10() {
-        double celcius = -10;
-        assertEquals(14.0, temperatureConverter.celsiusToFahrenheit(celcius));
-    }
-
-    @Test
-    void celsiusToFahrenheit_Negative15() {
-        double celcius = -15;
-        assertEquals(5.0, temperatureConverter.celsiusToFahrenheit(celcius));
-    }
-
-    @Test
-    void isExtremeTemperature_False_0() {
-        double celcius = 0;
-        assertFalse(temperatureConverter.isExtremeTemperature(celcius));
-    }
-
-    @Test
-    void isExtremeTemperature_False_20() {
-        double celcius = 20;
-        assertFalse(temperatureConverter.isExtremeTemperature(celcius));
-    }
-
-    @Test
-    void isExtremeTemperature_True_100() {
-        double celcius = 100;
-        assertTrue(temperatureConverter.isExtremeTemperature(celcius));
-    }
-
-    @Test
-    void isExtremeTemperature_True_50() {
-        double celcius = 50;
-        assertTrue(temperatureConverter.isExtremeTemperature(celcius));
-    }
-
-    @Test
-    void isExtremeTemperature_False_49_9() {
-        double celcius = 49.9;
-        assertFalse(temperatureConverter.isExtremeTemperature(celcius));
-    }
-
-    @Test
-    void isExtremeTemperature_True_Negative40() {
-        double celcius = -40;
-        assertTrue(temperatureConverter.isExtremeTemperature(celcius));
-    }
-
-    @Test
-    void isExtremeTemperature_False_39_9() {
-        double celcius = -39.9;
-        assertFalse(temperatureConverter.isExtremeTemperature(celcius));
+    @ParameterizedTest(name = "{0} Kelvin is {1} Celsius")
+    @CsvSource({ "273.15, 0.0", "273, -0.1", "200, -73.1", "250, -23.1", "0, -273.1", "111.1, -162.0" })
+    void kelvinToCelsius(double kelvin, double celcius) {
+        assertEquals(celcius, temperatureConverter.kelvinToCelsius(kelvin));
     }
 }
